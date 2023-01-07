9f391dc8e40b69c70d7641b6e8f77971640165aa
VladAir <vladair@MacBook-Air-VladAir.local>
2023-01-07 21:23:49 +0300

    fix 3.3

---
+++ src/Data/products.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/toothbrush.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: '/img/phone.jpg',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: '/img/board.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg',
+  },
+];
