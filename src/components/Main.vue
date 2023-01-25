f7bd3e3c1b5a73316af7108945441701ebdf1cc2
VladAir <vladair@MacBook-Air-VladAir.local>
2023-01-25 20:13:15 +0300

    add loading

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,6 +5,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
+      <div class="SEX"></div>
+        <LoadingVue v-if="productLoading" />
         <main class="content container">
             <div class="content__top content__top--catalog">
                 <h1 class="content__title">
@@ -14,12 +16,15 @@
                     {{ products.length }}
                 </span>
             </div>
+          <div v-if="productErrorData">
+          <h2>Ошибка соединения, плиз перезагрузите страницу ^_^</h2>
+          <button @click.prevent="loadProducts">попробовать еще раз</button>
+          </div>
             <div class="content__catalog">
                 <ProductFilterVue :category-color.sync="filterCategoryColor" :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo" :category-id.sync="filterCategoryId"/>
                 <ProductListVue :products="productss"/>
             </div>
             <BasePaginationVue v-model="page" :count="countProducts" :perPage="productsPerPage" :products="products" />
-
         </main>
     </div>
 </template>
@@ -30,6 +35,7 @@ import { API_URL } from '@/config';
 import ProductFilterVue from './ProductFilter.vue';
 import ProductListVue from './ProductList.vue';
 import BasePaginationVue from './BasePagination.vue';
+import LoadingVue from './pages/LoadingPage.vue';
 
 export default {
   data() {
@@ -41,6 +47,8 @@ export default {
       page: 1,
       productsPerPage: 6,
       productsData: null,
+      productLoading: false,
+      productErrorData: false,
     };
   },
   props: ['products'],
@@ -48,6 +56,7 @@ export default {
     ProductFilterVue,
     ProductListVue,
     BasePaginationVue,
+    LoadingVue,
   },
   computed: {
     productss() {
@@ -64,6 +73,8 @@ export default {
   },
   methods: {
     loadProducts() {
+      this.productLoading = true;
+      this.productErrorData = false;
       clearTimeout(this.loadProductsTimer);
       this.loadProductsTimer = setTimeout(() => {
         axios.get(`${API_URL}/api/products`, {
@@ -77,7 +88,11 @@ export default {
           },
         })
           // eslint-disable-next-line no-return-assign, no-shadow
-          .then((response) => this.productsData = response.data);
+          .then((response) => this.productsData = response.data)
+          // eslint-disable-next-line no-return-assign
+          .catch(() => this.productErrorData = true)
+          // eslint-disable-next-line no-return-assign
+          .then(() => this.productLoading = false);
       }, 0);
     },
   },
---
+++ src/components/pages/LoadingPage.vue
@@ -0,0 +1,58 @@
+<!-- eslint-disable max-len -->
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<div id="container">
+    <svg viewBox="0 0 100 100">
+        <defs>
+            <filter id="shadow">
+                <feDropShadow dx="0" dy="0" stdDeviation="1.5" flood-color="#272727" />
+            </filter>
+        </defs>
+        <circle id="spinner"
+            style="fill:transparent;stroke:rgb(152 242 11);;stroke-width: 7px;stroke-linecap: round;filter:url(#shadow);" cx="50"
+            cy="50" r="45" />
+    </svg>
+</div>
+
+</template>
+
+<style>
+/* div {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+} */
+
+#container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 200px;
+    height: 200px;
+}
+
+@keyframes animation {
+    0% {
+        stroke-dasharray: 1 98;
+        stroke-dashoffset: -105;
+    }
+
+    50% {
+        stroke-dasharray: 80 10;
+        stroke-dashoffset: -160;
+    }
+
+    100% {
+        stroke-dasharray: 1 98;
+        stroke-dashoffset: -300;
+    }
+}
+
+#spinner {
+    transform-origin: center;
+    animation-name: animation;
+    animation-duration: 1.2s;
+    animation-timing-function: cubic-bezier;
+    animation-iteration-count: infinite;
+}
+</style>
